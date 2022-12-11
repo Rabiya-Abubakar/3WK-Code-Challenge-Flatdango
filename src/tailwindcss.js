665de8849253c91137e4458a6c756c6216048d0f
Ayub Ian <iayub.mia@gmail.com>
2022-12-11 21:16:37 +0300

    Initial commit

---
+++ src/tailwindcss.js
@@ -0,0 +1,22 @@
+tailwind.config = {
+    theme: {
+      container: {
+        center: true,
+      },
+      extend: {
+        colors: {
+          clifford: '#da373d',
+          gila: '#007185',
+          stallon: '#F4F4F4',
+          kibidango: '#F5C518'
+        },
+        width: {
+          '168': '168px',
+          '188': '188px'
+        },
+        height: {
+          '500' : '540px'
+        }
+      }
+    }
+  }
